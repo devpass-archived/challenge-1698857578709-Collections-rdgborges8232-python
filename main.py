01d48ae3cdda451c005a585905b9f3bf4c69df1b
rodrigo-devpass <123122131+rodrigo-devpass@users.noreply.github.com>
2023-11-01 13:55:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from collections import Counter
+
 def count_duplicates(lst):
-    # Add your solution using collections here!
-    return -1
+    counts = Counter(lst)
+    duplicates = sum(count - 1 for count in counts.values())
+    return duplicates
 
 if __name__ == '__main__':
     numbers = [1, 3, 4, 2, 5, 3, 4, 1, 1]
